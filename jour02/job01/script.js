f42b23c46add52d9c4bea3e5f887d4978ecbf3b8
samuel-corinthe <samuel.corinthe@hotmail.fr>
2025-10-17 09:59:25 +0200

    commit jour2 job1

---
+++ jour02/job01/script.js
@@ -0,0 +1,10 @@
+function citation()
+{
+    const boutton = document.querySelector("#button");
+    const citation = document.querySelector("#citation");
+
+    boutton.addEventListener("click", () => { console.log (citation.textContent);
+
+    });
+}
+citation();
