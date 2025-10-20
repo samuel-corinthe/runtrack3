3e07571dd8d7e35dffcaf4e54d74d6ad9a190d7b
samuel-corinthe <samuel.corinthe@hotmail.fr>
2025-10-20 14:02:10 +0200

    commit jour4  job2

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,10 +1,11 @@
-jsonValueKey({
-name: "La Plateforme_",
-address: "8 Terasse Bellini",
-city: "Paris",
-nb_staff: "11",
-creation:"2024"
-});
 
-jsonObject=JSON.parse(jsonValueKey);
+function jsonValueKey(adress, key)
+{
+return adress[key];
+} 
+const adress = `{"name": "La Plateforme_","address": "8 Terasse Bellini","city": "Paris","nb_staff": "11","creation":"2024"}`;
+
+object = JSON.parse(adress);
+
+console.log(jsonValueKey(object, "nb_staff"));
 
